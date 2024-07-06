fbe428c9748eb46bd6539e00267e010ecad5ba46
PraeThipwarin <prae.tippy@gmail.com>
2024-07-06 11:13:18 +0700

    q2 again

--- q2.js
+++ q2.js
@@ -1,13 +1,20 @@
 // assign interface/type to the function definition properly
-function findTopNames(students) {
-    var TopStudent = students.filter(function (x) { return x.score > 8; });
-    return TopStudent;
+class Student {
+    name;
+    score;
 }
+
+function findTopNames(students){
+    let TopStudent = students.filter((x) => x.score > 8 );
+    return TopStudent;
+  }
+  
 // assign interface/type to the student1 object properly
-var students1 = [
+const students1 = [
     { name: "john", score: 10 },
     { name: "jane", score: 9 },
     { name: "jim", score: 8 },
-];
+  ];
+  
 console.log(findTopNames(students1));
 module.exports = findTopNames;
